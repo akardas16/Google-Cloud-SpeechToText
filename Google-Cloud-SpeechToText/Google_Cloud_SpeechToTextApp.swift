8708472df147374edbaa876d5590fe741692fd1b
Abdullah Kardaş <krdsabdllh53@gmail.com>
2024-06-20 14:02:13 +0300

    Initial Commit

---
+++ Google-Cloud-SpeechToText/Google_Cloud_SpeechToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_Cloud_SpeechToTextApp.swift
+//  Google-Cloud-SpeechToText
+//
+//  Created by Abdullah Kardaş on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Google_Cloud_SpeechToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
